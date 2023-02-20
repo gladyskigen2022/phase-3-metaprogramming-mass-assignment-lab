b8fd94197ed0893b9a67b510c36d424fd3eeec9e
GladysKigen <gladys.kigen@student.moringa.com>
2023-02-20 22:18:15 +0300

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
 class Person
   # your code here
+  # dynamically setting getters and setters/we can now get and set every attribute on an object instantiated from Person
+  
+  def initialize(attributes)
+    attributes.each do |key, value|
+      # create a getter and setter by calling the attr_accessor method
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+
